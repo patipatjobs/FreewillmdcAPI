d3c05a834439fadc044a194d8af10110896180f7
patipatjobs <46826241+patipatjobs@users.noreply.github.com>
2020-11-22 12:36:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,25 +2,6 @@
 const sha256 = require('sha256');
 const moment = require('moment');
 const momentz = require('moment-timezone');
-// var cron = require('node-cron');
-
-////////////////////////////////////////////////////////
-
-// const connection = mysql.createConnection({   
-//     host     : 'freewillmdc.loginto.me', 
-//     port     : '56860',
-//     user     : 'fwghr',
-//     password : 'fwg@mdc04111',
-//     database : 'fwg_hr'
-// });
-
-/////////////////////////////////////////////////////////
-
-// const time_at = moment().tz('Asia/Bangkok').format("YYYY-MM-DD HH:mm:ss");
-// const dateToday = moment().tz('Asia/Bangkok').format("YYYY-MM-DD");
-
-///////////////////////////////////////////////////////
-
 const middleware = (req, res, next) => {
 
     var chkmobile = sha256('@MDC');
@@ -33,25 +14,6 @@ const middleware = (req, res, next) => {
         res.json("Access Denied")
 
 }; 
-
-////////////////////////////////////////////////////////
-
-// const cronJob = () => { 
-
-//     console.log("Start Cron");
-
-//     cron.schedule('*/2 * * * *', function (a) { 
-    
-//         const time_at = moment().tz('Asia/Bangkok').format("YYYY-MM-DD HH:mm:ss");
-
-//         console.log("---------------------");
-//         console.log('Running Cron Job '+time_at);
- 
-//     });
-
-// };
-
-////////////////////////////////////////////////////////
 
 module.exports = {
     middleware
